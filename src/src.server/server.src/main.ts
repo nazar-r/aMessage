b05c11d54116fc4a58dfd4b859158c2b3ba2ff53
nazar-r <nazik2008ua@gmail.com>
2026-04-06 15:42:18 +0000

    Back Updated

--- src/src.server/server.src/main.ts
+++ src/src.server/server.src/main.ts
@@ -1,20 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
-import * as passport from 'passport';
-import * as cookieParser from 'cookie-parser';
+import { createClient } from 'redis';
+import session from 'express-session';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import RedisStore from 'connect-redis';
 import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  const redisClient = createClient({
+    url: 'redis://localhost:6379',
+  });
+
+  redisClient.on('error', (err) => {
+    console.error('Redis error:', err);
+  });
+
+  await redisClient.connect();
+  const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: 'sess:',
+  });
 
   app.use(
     session({
-      secret: process.env.JWT_SECRET,
+      store: redisStore,
+      secret: process.env.JWT_SECRET as string,
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60 * 24,
+      },
     }),
   );
 
