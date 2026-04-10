af03c775f871a16ca86501db2c95f51435347353
nazar-r <nazik2008ua@gmail.com>
2026-04-10 13:52:07 +0000

    Front Updated

--- src/src.app/app.src/src.a.tsx/tsx.pages/welcome.page.tsx
+++ src/src.app/app.src/src.a.tsx/tsx.pages/welcome.page.tsx
@@ -1,30 +1,66 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const WelcomePageContent = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const handleBegin = async () => {
+    if (loading) return; 
+ setLoading(true);
+    try {
+      const res = await fetch("https://api.amessage.site/auth/check", {
+        credentials: "include",
+      });
+
+      const data = await res.json();
+      await new Promise(r => setTimeout(r, 300));
+
+      if (data.user) {
+        navigate("/chat-prev");
+      } else {
+        navigate("/login");
+      }
+
+    } catch (err) {
+      await new Promise(r => setTimeout(r, 300));
+      navigate("/login");
+    }
+  };
 
   return (
     <div className="welcome-page">
       <div className="welcome-page-container">
         <div className="welcome-page-container__title">Welcome Here!</div>
-        <div className="welcome-page-container__description">... in E2E encrypted aMessage</div>
+        <div className="welcome-page-container__description">
+          ... in E2E encrypted aMessage
+        </div>
       </div>
 
-      <div className="welcome-page__button" onClick={async () =>
-        (await fetch("https://api.amessage.site/auth/check", { credentials: "include" }).then(res => res.json()).then(data => data.user))
-          ? navigate("/chat-prev")
-          : navigate("/login")
-      }
+      <div
+        className={`welcome-page__button ${loading ? "is-loading" : ""}`}
+        onClick={handleBegin}
       >
         <div className="welcome-page__button--title__container">
-          <div className="welcome-page__button--title">Begin</div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="login-page__button--icon">
-            <line x1="5" y1="12" x2="19" y2="12" />
-            <polyline points="12 5 19 12 12 19" />
-          </svg>
+          <div className="welcome-page__button--title"> Begin</div>
+
+          <div className={loading ? "loader" : ""}>
+            {!loading && (
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                className="login-page__button--icon">
+                <line x1="5" y1="12" x2="19" y2="12" />
+                <polyline points="12 5 19 12 12 19" />
+              </svg>
+            )}
+          </div>
         </div>
-        <div className="welcome-page__button--title__spec">NOTE: The first launch may take up to 50 seconds for the server to start.</div>
+
+        <div className="welcome-page__button--title__spec">
+    NOTE: The first launch may take up to 50 seconds for the server to start
+        </div>
       </div>
+
+
     </div>
   );
 };
