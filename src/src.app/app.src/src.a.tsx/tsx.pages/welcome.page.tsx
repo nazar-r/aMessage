6eb05b3ac787393bde00edc96d9f460fa04c0dfb
nazar-r <nazik2008ua@gmail.com>
2026-05-10 11:01:14 +0000

    Cookies Updated

--- src/src.app/app.src/src.a.tsx/tsx.pages/welcome.page.tsx
+++ src/src.app/app.src/src.a.tsx/tsx.pages/welcome.page.tsx
@@ -8,23 +8,24 @@ const WelcomePageContent = () => {
   const handleBegin = async () => {
     if (loading) return;
     setLoading(true);
+    
+    const delay = () => new Promise(resolve => setTimeout(resolve, 300));
+    const loginPath = "/login";
+    const chatsPath = "/chatslist";
+
     try {
-      const res = await fetch(import.meta.env.VITE_FETCH_MESSAGES_URL, {
+      const res = await fetch(import.meta.env.VITE_AUTH_CHECK_URL, {
         credentials: "include",
       });
 
       const data = await res.json();
-      await new Promise(r => setTimeout(r, 300));
+      await delay();
 
-      if (data.user) {
-        navigate("/chat-prev");
-      } else {
-        navigate("/login");
-      }
+      navigate(data.user ? chatsPath : loginPath);
+    } catch {
+      await delay();
 
-    } catch (err) {
-      await new Promise(r => setTimeout(r, 300));
-      navigate("/login");
+      navigate(loginPath);
     }
   };
 
@@ -32,25 +33,16 @@ const WelcomePageContent = () => {
     <div className="welcome-page">
       <div className="welcome-page-container">
         <div className="welcome-page-container__title">Welcome Here!</div>
-        <div className="welcome-page-container__description">
-          ... in E2E encrypted aMessage
+        <div className="welcome-page-container__description">aMessage - secure chatting
         </div>
       </div>
 
-      <div
-        className={`welcome-page__button ${loading ? "is-loading" : ""}`}
-        onClick={handleBegin}
-      >
+      <div className={`welcome-page__button ${loading ? "is-loading" : ""}`} onClick={handleBegin}>
         <div className="welcome-page__button--title__container">
-          <div className="welcome-page__button--title"> Begin</div>
-
+          <div className="welcome-page__button--title">Begin</div>
           <div className={loading ? "loader" : ""}>
             {!loading && (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                className="login-page__button--icon">
-                <line x1="5" y1="12" x2="19" y2="12" />
-                <polyline points="12 5 19 12 12 19" />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="login-page__button--icon"><line x1="5" y1="12" x2="19" y2="12" /><polyline points="12 5 19 12 12 19" /> </svg>
             )}
           </div>
         </div>
@@ -58,13 +50,10 @@ const WelcomePageContent = () => {
         {/* <div className="welcome-page__button--title__spec-1">
           The app is currently being updated
         </div> */}
-
-        <div className="welcome-page__button--title__spec">
+        {/* <div className="welcome-page__button--title__spec">
           NOTE: The first launch may take up to 50 seconds for the server to start
-        </div>
+        </div> */}
       </div>
-
-
     </div>
   );
 };
